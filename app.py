8d2daa0ba67458756928b42968e5970400e0ff26
t125063 <t125063@ed.sus.ac.jp>
2026-01-27 14:52:49 +0900

    wip: 絞り込み条件「年」を追加

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+st.title("日本の人口の推移")
+
+df = pd.read_csv("./population_trends.csv")
+header_cols = df.columns[1:]
+col_series = pd.Series(header_cols)
+
+with st.sidebar:
+    st.header("絞り込み条件")
+    select_year = st.selectbox(
+        "年を選択",
+        options=col_series.str.split('_').str[0].unique()
+    )
