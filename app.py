4a2f301b462e31a8f20aeaabdb30fd7489101de0
t125063 <t125063@ed.sus.ac.jp>
2026-02-02 02:03:41 +0900

    wip: save

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from streamlit_echarts import st_echarts
 
 # --- ページ設定 ---
 st.set_page_config(page_title="日本の人口推移アプリ", layout="wide")
